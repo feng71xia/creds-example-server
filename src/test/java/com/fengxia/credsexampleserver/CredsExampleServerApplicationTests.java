328cb1232f55f6d6dd5cfde1dc5233928f85ae1c
feng71xia <32876174+feng71xia@users.noreply.github.com>
2021-01-31 12:04:52 -0500

    Initial commit

---
+++ src/test/java/com/fengxia/credsexampleserver/CredsExampleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengxia.credsexampleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredsExampleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
